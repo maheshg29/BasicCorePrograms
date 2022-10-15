b125e0b7f9deebeb010bc72e2ac8be893dcafd1e
mahesh <gangurdemahesh9@gmail.com>
2022-10-15 18:58:23 +0530

    [mahesh] create assembly with welcome message

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class BasicCoreProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Basic Core Program Day 5 Assingment");
+    }    
+}
+
+
+
+
+
+
