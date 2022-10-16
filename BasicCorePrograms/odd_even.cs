8471f689689e57826f7bcfbe34a7eb29fffd0bae
mahesh <gangurdemahesh9@gmail.com>
2022-10-16 17:02:52 +0530

    [mahesh]add check odd even program

---
+++ BasicCorePrograms/odd_even.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class odd_even
+    {
+        public static void check_odd_even()
+        {
+            
+            Console.Write("Enter a Number : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+                Console.Write("Entered Number is an Even Number");
+            
+            else
+                Console.Write("Entered Number is an Odd Number");
+              
+        }
+    }
+}
