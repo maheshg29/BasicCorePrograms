36b2c80edb3144f9a01e19418ee6972843e8dca1
mahesh <gangurdemahesh9@gmail.com>
2022-10-16 16:48:08 +0530

    [mahesh]add quot and reminder program

---
+++ BasicCorePrograms/quotient_remainder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class quotient_remainder
+    {
+        public static void checkquot_remd()
+        {
+            Console.WriteLine("Enter divident value");
+            int dividend =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor value");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+           
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+           
+        }
+    }
+}
