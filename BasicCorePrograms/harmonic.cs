effa7feb7da4ed419b745e6224f5b0676e013b38
mahesh <gangurdemahesh9@gmail.com>
2022-10-16 15:59:31 +0530

    [mahesh]add harmonic series program

---
+++ BasicCorePrograms/harmonic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class harmonic
+    {
+        public static void checkharmonic()
+        {
+            int i, num;
+            double sum = 0;
+            Console.WriteLine("Enter the Number for Harmonic series");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= num; i++)
+            {
+                Console.WriteLine("1/{0} +", i);
+                sum = sum + 1 / (float)i;
+            }
+            Console.WriteLine("sum of series upto {0} terms :{1}", num, sum);
+        }
+    }
+}
