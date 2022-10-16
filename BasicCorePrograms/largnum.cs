7d7ad4c210a0f4f161633c920a41f76614ea9c8b
mahesh <gangurdemahesh9@gmail.com>
2022-10-16 17:21:37 +0530

    [mahesh]add find large number program

---
+++ BasicCorePrograms/largnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class largnum
+    {
+        public static void checklargnum()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Enter 1st Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd Number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest!\n");
+                }
+                else
+                {
+                    Console.Write("Number three is the largest!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the largest!\n");
+            else
+                Console.Write("Number three is the largest!\n");
+        }
+    }
+    
+}
