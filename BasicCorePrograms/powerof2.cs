41005690c3675ddcfb61e892021dbaaaa6f1f7d1
mahesh <gangurdemahesh9@gmail.com>
2022-10-16 15:53:35 +0530

    [mahesh]add power of 2 table

---
+++ BasicCorePrograms/powerof2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class powerof2
+    {
+        public static void powerof2table()
+        {
+            Console.WriteLine("Enter the number for power of 2 table");
+            int num = Convert.ToInt32(Console.ReadLine());
+            double pow;
+            for (int i = 1; i <= num; i++)
+            {
+                pow = Math.Pow(2, i);
+                Console.WriteLine(pow);
+            }
+        }
+    }
+}
