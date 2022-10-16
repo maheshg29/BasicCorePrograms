52d84319226d0aa4866ebef9516dd0d98649d2f8
mahesh <gangurdemahesh9@gmail.com>
2022-10-16 16:39:25 +0530

    [mahesh]add prime factor program

---
+++ BasicCorePrograms/primefactor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class primefactor
+    {
+        public static void checkprimefactor()
+        {
+            Console.WriteLine("Enter number");
+            int n = Convert.ToInt32(Console.ReadLine());
+           
+                while (n % 2 == 0)
+                {
+                    Console.WriteLine(2 + " ");
+                    n /= 2;
+                }
+                for (int i = 3; i * i <= Math.Sqrt(n); i += 2)
+                {
+                   
+                    while (n % i == 0)
+                    {
+                        Console.WriteLine(i + " ");
+                        n /= i;
+                    }
+                }
+                    if (n > 2)
+                    Console.Write(n);
+            
+        }
+    }
+}
